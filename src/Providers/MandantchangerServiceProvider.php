a5b55d0d21f99787b98ab6b1b7c9cb6cb2c2003f
xzite92@googlemail.com <xzite92@googlemail.com>
2020-05-07 21:30:21 +0200

    Go

---
+++ src/Providers/MandantchangerServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+ 
+namespace Mandantchanger\Providers;
+ 
+ 
+use Plenty\Plugin\ServiceProvider;
+ 
+class MandantchangerServiceProvider extends ServiceProvider
+{
+ 
+    /**
+     * Register the service provider.
+     */
+     
+    public function register()
+    {
+        $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+    }
+}
